36ec8c00ee9fd7b521177bd646f56c3fc8cc9c09
frakkin64 <258024685+frakkin64@users.noreply.github.com>
2026-05-19 09:19:40 -0700

    feat: multi-runtime build

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 CSimpleIniA ini(true, false, false);
 RE::BGSSoundCategory* playerSoundCategory;
 float playerVoiceFrequency = 1.0f;
-REL::Relocation<uintptr_t> ptr_changeTime{ REL::ID(2237201), 0x2B };
-REL::Relocation<uintptr_t> ptr_revertTime{ REL::ID(2237201), 0x6B };
-REL::Relocation<uintptr_t> ptr_overrideJump{ REL::ID(2267454), 0x97 };
-REL::Relocation<float*> ptr_globalTimeMultiplier{ REL::ID(2666308) };
+REL::Relocation<uintptr_t> ptr_changeTime{ REL::ID { 249054, 2237201 }, REL::Offset { 0x2B, 0x2B } };
+REL::Relocation<uintptr_t> ptr_revertTime{ REL::ID { 249054, 2237201 }, REL::Offset { 0x6B, 0x6B }  };
+REL::Relocation<uintptr_t> ptr_overrideJump{ REL::ID { 157156, 2267454 }, REL::Offset { 0x97, 0x97 } };
+REL::Relocation<float*> ptr_globalTimeMultiplier{ REL::ID { 1266509, 2666308 } };
 
 void SetPlayerVoiceFrequency([[maybe_unused]] float currentTime) {
 	ini.LoadFile("Data\\F4SE\\Plugins\\PlayerVoiceFrequency.ini");
@@ -23,7 +23,7 @@ void SetPlayerVoiceFrequency([[maybe_unused]] float currentTime) {
 
 void SetGlobalTimeMultiplier(uint64_t unk, float f) {
 	typedef void (*func_t)(uint64_t, float);
-	REL::Relocation<func_t> func{ REL::ID(2267970) };
+	REL::Relocation<func_t> func{ REL::ID { 1419977, 2267970 } };
 	func(unk, f);
 	SetPlayerVoiceFrequency(f);
 }
@@ -36,6 +36,22 @@ struct MenuWatcher : public RE::BSTEventSink<RE::MenuOpenCloseEvent> {
 		return RE::BSEventNotifyControl::kContinue;
 	}
 };
+
+F4SE_PLUGIN_QUERY(const F4SE::QueryInterface* a_f4se, F4SE::PluginInfo* a_info)
+{
+	if (const auto data = F4SE::PluginVersionData::GetSingleton()) {
+		a_info->infoVersion = F4SE::PluginInfo::kVersion;
+		a_info->name = data->GetPluginName().data();
+		a_info->version = data->GetPluginVersion().pack();
+	}
+
+	const auto ver = a_f4se->RuntimeVersion();
+	if (ver < REL::Version(F4SE::RUNTIME_1_10_163)) {
+		return false;
+	}
+
+	return true;
+}
 
 F4SE_PLUGIN_LOAD(const F4SE::LoadInterface* a_f4se)
 {
